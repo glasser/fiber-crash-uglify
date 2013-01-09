ad2d5c0e07320cbb71be60ad831be9939d9cd4d4
David Glasser <glasser@davidglasser.net>
2013-01-08 16:14:22 -0800

    more times

--- crash.js
+++ crash.js
@@ -4,4 +4,6 @@ Fiber(function() {
   var UglifyJS = require('uglify-js');
   UglifyJS.minify('sockjs-0.3.4.js');
   UglifyJS.minify('sockjs-0.3.4.js');
+  UglifyJS.minify('sockjs-0.3.4.js');
+  UglifyJS.minify('sockjs-0.3.4.js');
 }).run();
